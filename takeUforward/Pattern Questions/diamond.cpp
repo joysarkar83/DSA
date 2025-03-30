1c3436a1efa622ed5ae7da58befcd35bd592a6b3
Joy Sarkar <joysarkar0538@gmail.com>
2025-03-30 14:05:14 +0530

    Update diamond.cpp

--- takeUforward/Pattern Questions/diamond.cpp
+++ takeUforward/Pattern Questions/diamond.cpp
@@ -1,11 +1,30 @@
 #include <iostream>
 using namespace std;
 int main(){
-    int height;
+    int height, halfHeight;
     cout<<"Enter height: ";
     cin>>height;
+    halfHeight = height / 2;
 
-    for(int )
+    for(int k=1; k<=halfHeight; k++){
+        for(int i=halfHeight-k ; i>0 ; i--){
+            cout<<" ";
+        }
+        for(int j=1; j<=((2*k)-1); j++){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+
+    for(int k=halfHeight; k>0; k--){
+        for(int i=halfHeight-k; i>0; i--){
+            cout<<" ";
+        }
+        for(int j=((2*k)-1); j>0; j--){
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
 
     return 0;
 }
