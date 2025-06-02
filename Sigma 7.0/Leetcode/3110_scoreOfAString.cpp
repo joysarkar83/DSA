230b9fc2da9285f43adb2cb81a27d1a8f0a45a7a
Joy Sarkar <joysarkar0538@gmail.com>
2025-06-02 23:25:46 +0530

    Reallocation

---
+++ Sigma 7.0/Leetcode/3110_scoreOfAString.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int scoreOfString(string s) {
+    unsigned int sum = 0;
+    for(int i=0; i<s.size()-1; i++){
+        sum += abs((s[i]-'0') - (s[i+1]-'0'));
+    }
+    return sum;
+}
+
+int main(){
+
+    cout<<scoreOfString("zaz");
+
+    return 0;
+}
