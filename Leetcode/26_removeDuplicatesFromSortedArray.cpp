2241db28870429035699e6affca75e36f9cef977
Joy Sarkar <joysarkar0538@gmail.com>
2025-06-05 01:40:06 +0530

    Create 26_removeDuplicatesFromSortedArray.cpp

---
+++ Leetcode/26_removeDuplicatesFromSortedArray.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int removeDuplicates(vector<int>& nums){
+    int preIdx = 0, postIdx = 0, n = nums.size();
+    for(preIdx; preIdx<n; preIdx++){
+        if(nums[preIdx] != nums[postIdx]){
+            nums[++postIdx] = nums[preIdx];
+        }
+    }
+    for(postIdx; postIdx<n-1; postIdx++){
+        nums.pop_back();
+    }
+    return nums.size();
+}
+
+int main(){
+    vector<int> arr = {0,0,1,1,1,2,2,3,3,3,4,7,8};
+
+    cout<<removeDuplicates(arr)<<"\n";
+
+    for(int i:arr){
+        cout<<i<<" ";
+    }
+
+    return 0;
+}
