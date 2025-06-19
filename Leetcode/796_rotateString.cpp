a1a5d5188aead7ab813b3884bd6f389079e0a02b
Joy Sarkar <joysarkar0538@gmail.com>
2025-06-19 23:59:13 +0530

    Update 796_rotateString.cpp

--- Leetcode/796_rotateString.cpp
+++ Leetcode/796_rotateString.cpp
@@ -3,34 +3,26 @@
 #include <string>
 using namespace std;
 
-bool rotateString(string s, string goal) {
-    int n = s.size(), m=goal.size();
+bool rotateString(string& s, string goal) {
+    int n = s.size();
 
-    if(n != m){
+    if(n != goal.size()){
         return false;
     }
 
-    bool matchFound = false;
-    for(int i=0; i<n; i++){  //main string first letter
-        for(int j=0; j<m; j++){
-            if(s[i] == goal[j] && matchFound == false && i<n){
-                matchFound = true;
-                i++; j++;
-                if(j==m && matchFound == true){
-                    j = 0;
-                }
-            }
-            else if(s[i] != goal[j] && matchFound == true && i<n){
-                return false;
-            }
+    for(int i=0; i<n; i++){
+        s.insert(s.begin(), s.back());
+        s.pop_back();
+        if(s == goal){
+            return true;
         }
     }
-    return true;
+    return false;
 }
 
 int main(){
-    string str = "abcde";
-    string goal = "cdeab";
+    string str = "gcmbf";
+    string goal = "fgcmb";
 
     cout<<rotateString(str, goal);
 
