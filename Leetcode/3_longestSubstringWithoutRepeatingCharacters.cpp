8b8e915bcee07b7b4612cc3cbc0fed7776304fc5
Joy Sarkar <joysarkar0538@gmail.com>
2025-06-21 02:20:41 +0530

    Update 3_longestSubstringWithoutRepeatingCharacters.cpp

--- Leetcode/3_longestSubstringWithoutRepeatingCharacters.cpp
+++ Leetcode/3_longestSubstringWithoutRepeatingCharacters.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
 int lengthOfLongestSubstring(string s){
-    int n = s.size(), count = 0;
-    vector<int> track;
-    for(int i=0; i<n-1; i++){
-        for(int j=i; j<n; j++){
-            track[(int)s[j]]++;
-            
-            if(s[i] == s[j]){
-                currCount = 1;
-                break;
-            }
-            else if((int)s[j] <= 1)
+    int n = s.size(), count = 0, start = 0;
 
-
-
+    vector<bool> track(256, false);
+    
+    for(int end=0; end<n; end++){
+        while(track[s[end]]){
+            track[s[start]] = false;
+            start++;
         }
+        
+        track[s[end]] = true;
+        count = max(count, end - start + 1);
     }
+    return count;
 }
 
 int main(){
+    string str = "pwwkew";
+
+    cout<<lengthOfLongestSubstring(str);
 
     return 0;
 }
