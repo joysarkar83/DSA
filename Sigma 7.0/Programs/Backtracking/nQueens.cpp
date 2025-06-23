b026be0e861326e84785fc35856b5a083fcf79b2
Joy Sarkar <joysarkar0538@gmail.com>
2025-06-23 22:06:43 +0530

    Update nQueens.cpp

--- Sigma 7.0/Programs/Backtracking/nQueens.cpp
+++ Sigma 7.0/Programs/Backtracking/nQueens.cpp
@@ -4,35 +4,52 @@ using namespace std;
 
 void printBoard(vector<vector<char>>& board){
     int n = board.size();
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cout<<board[i][j]<<" ";
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cout << board[i][j] << " ";
         }
-        cout<<"\n";
+        cout << "\n";
     }
-    return;
+    cout << "-----------------------\n";
 }
 
-void nQueensAllCombinations(vector<vector<char>> board, int n, int col){
-    if(col == n){
+bool isSafe(vector<vector<char>>& board, int n, int row, int col){
+    // Check vertical column
+    for(int i = 0; i < row; i++){
+        if(board[i][col] == 'Q') return false;
+    }
+
+    // Check upper-left diagonal
+    for(int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--){
+        if(board[i][j] == 'Q') return false;
+    }
+
+    // Check upper-right diagonal
+    for(int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++){
+        if(board[i][j] == 'Q') return false;
+    }
+
+    return true;
+}
+
+void nQueens(vector<vector<char>>& board, int n, int row){
+    if(row == n){
         printBoard(board);
-        cout<<"-----------------------\n";
         return;
     }
 
-    for(int i=0; i<n; i++){
-        board[i][col] = 'Q';
-        nQueensAllCombinations(board, n, col+1);
-        board[i][col] = '.';
+    for(int col = 0; col < n; col++){
+        if(isSafe(board, n, row, col)){
+            board[row][col] = 'Q';
+            nQueens(board, n, row + 1);
+            board[row][col] = '.';  // Backtrack
+        }
     }
-    return;
 }
 
 int main(){
-    int nRows = 3; 
-    vector<vector<char>> board(nRows, vector<char>(nRows, '.'));
-
-    nQueensAllCombinations(board, nRows, 0);
-
+    int n = 4;
+    vector<vector<char>> board(n, vector<char>(n, '.'));
+    nQueens(board, n, 0);
     return 0;
 }
