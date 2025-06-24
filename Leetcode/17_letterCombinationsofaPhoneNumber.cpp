c98838fda961246bd52f494d50275e2f8c502c53
Joy Sarkar <joysarkar0538@gmail.com>
2025-06-25 02:51:50 +0530

    Update 17_letterCombinationsofaPhoneNumber.cpp

--- Leetcode/17_letterCombinationsofaPhoneNumber.cpp
+++ Leetcode/17_letterCombinationsofaPhoneNumber.cpp
@@ -2,28 +2,28 @@
 #include <vector>
 #include <string>
 using namespace std;
-void generate(vector<string>& final, int currDigIndex, string currString, string digits){
-    vector<string> data = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-
+void generate(vector<string>& final, int currDigIndex, string currString, string digits, vector<string>& data){
+    
     if(currDigIndex == digits.length()){
         final.push_back(currString);
         return;
     }
-
+    
     int digit = digits[currDigIndex] - '0';
     for(int i = 0; i < data[digit].length(); i++){
-        generate(final, currDigIndex + 1, currString + data[digit][i], digits);
+        generate(final, currDigIndex + 1, currString + data[digit][i], digits, data);
     }
-
+    
     return;
 }
 
-vector<string> letterCombinations(string digits){
+vector<string> letterCombinations(string& digits){
     vector<string> final;
     if(digits.length() == 0){
         return final;
     }
-    generate(final, 0, "", digits);
+    vector<string> data = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    generate(final, 0, "", digits, data);
     return final;
 }
 
