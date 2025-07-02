965869fd9a37e3a61bf16f5388bef35cb41b8646
Joy Sarkar <joysarkar0538@gmail.com>
2025-07-02 22:33:24 +0530

    Create 191_numberOf1Bits.cpp

---
+++ Leetcode/191_numberOf1Bits.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int hammingWeight(int n) {
+    int count = 0;
+    while(n>0){
+        if(n%2 == 1){
+            count++;
+        }
+        n = n/2;
+    }
+    return count;
+}
+
+int main(){
+    return 0;
+}
