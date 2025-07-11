527546a6eb9fe0347a05a21ae84711dcdb2da28b
Joy Sarkar <joysarkar0538@gmail.com>
2025-07-12 03:32:14 +0530

    Update 2073_timeNeededToBuyTickets.cpp

--- Leetcode/2073_timeNeededToBuyTickets.cpp
+++ Leetcode/2073_timeNeededToBuyTickets.cpp
@@ -3,39 +3,65 @@
 #include <vector>
 using namespace std;
 
+//Optimised - Observing pattern
 int timeRequiredToBuy(vector<int>& tickets, int k) {
-    //Preparing queue
-    queue<int> line;
-    for(int i:tickets){
-        line.push(i);
-    }
-
     int time = 0;
     int kVal = tickets[k];
 
-    //queue is ready
-    while(!line.empty() && kVal>0){
-        int f = line.front();
-        if(f-1 != 0){
-            line.push(f-1);
+    for(int i=0; i<tickets.size(); i++){
+        int currVal = tickets[i];
+        if(i<k){
+            time += min(currVal, kVal);
         }
-        line.pop();
-        time++;
-        k--;
-        
-        if(k < 0){
-            k = line.size()-1;
-            kVal--;
+        else if(i==k){
+            time += currVal;
+        }
+        else{
+            if(currVal < kVal){
+                time += currVal;
+            }
+            else{
+                time += kVal-1;
+            }
         }
     }
     return time;
 }
 
+
+//Brute Force
+// int timeRequiredToBuy(vector<int>& tickets, int k) {
+//     //Preparing queue
+//     queue<int> line;
+//     for(int i:tickets){
+//         line.push(i);
+//     }
+
+//     int time = 0;
+//     int kVal = tickets[k];
+
+//     //queue is ready
+//     while(!line.empty() && kVal>0){
+//         int f = line.front();
+//         if(f-1 != 0){
+//             line.push(f-1);
+//         }
+//         line.pop();
+//         time++;
+//         k--;
+        
+//         if(k < 0){
+//             k = line.size()-1;
+//             kVal--;
+//         }
+//     }
+//     return time;
+// }
+
 int main(){
-    vector<int> tickets = {5,1,1,1};
+    vector<int> tickets = {4,3,5,6,4,5};
 
-    cout<<timeRequiredToBuy(tickets, 0);
-
-
+    cout<<timeRequiredToBuy(tickets, 2);
+    
     return 0;
 }
