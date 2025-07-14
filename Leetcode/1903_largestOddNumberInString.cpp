b1510699f9bc77fb12cd76e160b01bca68d84e6f
Joy Sarkar <joysarkar0538@gmail.com>
2025-07-15 03:45:44 +0530

    Create 1903_largestOddNumberInString.cpp

---
+++ Leetcode/1903_largestOddNumberInString.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string largestOddNumber(string& num) {
+    int n = num.size();
+
+    for(int i=n-1; i>=0; i--){
+        if((num[i] - '0') % 2 == 1){
+            while(i != -1){
+                return num.substr(0, i+1);
+            }
+        }
+    }
+    return "";
+}
+
+int main(){
+    string s = "10133890";
+
+    cout<<largestOddNumber(s);
+
+    return 0;
+}
