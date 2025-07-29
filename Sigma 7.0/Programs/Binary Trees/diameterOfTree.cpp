2bfccfe012524095e117ed5a118403c83c648c45
Joy Sarkar <joysarkar0538@gmail.com>
2025-07-29 22:41:34 +0530

    Create diameterOfTree.cpp

---
+++ Sigma 7.0/Programs/Binary Trees/diameterOfTree.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class TreeNode{
+public:
+    int val;
+    TreeNode *left, *right;
+
+    TreeNode(int val){
+        this->val = val;
+        left = right = NULL;
+    }
+};
+
+static int idx = -1;
+TreeNode* buildTree(vector<int> nodes){
+    idx++;
+    if(nodes[idx] == -1) return NULL;
+
+    TreeNode* currNode = new TreeNode(nodes[idx]);
+    currNode->left = buildTree(nodes);
+    currNode->right = buildTree(nodes);
+    return currNode;
+}
+
+// APPROACH 1 - 0(n^2)
+// int treeHeight(TreeNode* root){
+//     if(root == NULL) return 0;
+
+//     int leftHt = treeHeight(root->left);
+//     int rightHt = treeHeight(root->right);
+
+//     return max(leftHt, rightHt) + 1;
+// }
+
+// int treeDiameter(TreeNode* root){
+//     if(root == NULL) return 0;
+
+//     int leftDiam = findDiameter(root->left);
+//     int rightDiam = findDiameter(root->right);
+//     int currDiam = treeHeight(root->left) + treeHeight(root->right) + 1;
+
+//     return max(currDiam, max(leftDiam, rightDiam));
+// }
+
+// APPROACH 2 - 0(n)
+pair<int,int> treeDiameter(TreeNode* root){
+
+}
+
+int main(){
+    vector<int> nodes = {10, 5, 2, -1, -1, 7, 6, -1, -1, 8, -1, -1, 15, 12, 11, -1, -1, 13, -1, -1, 18, 17, -1, -1, 20, -1, -1};
+
+    TreeNode* root = buildTree(nodes);
+
+    // cout<<treeHeight(root);
+    cout<<treeDiameter(root).first;
+
+    return 0;
+}
