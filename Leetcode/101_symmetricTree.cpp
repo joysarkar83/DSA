3b682d131928692a62eb754e21bed2af8cda399d
Joy Sarkar <joysarkar0538@gmail.com>
2025-08-03 17:16:36 +0530

    Update 101_symmetricTree.cpp

--- Leetcode/101_symmetricTree.cpp
+++ Leetcode/101_symmetricTree.cpp
@@ -24,8 +24,41 @@ TreeNode* buildTree(vector<int>& nodes){
     return currNode;
 }
 
-bool isSymmetric(TreeNode* root) {
+// FXN 1: Get left subtree vals as a vector - left first
+void getLeftSubTree(TreeNode* root, vector<int>& leftSubTree){
+    if(root == NULL){
+        leftSubTree.emplace_back(-101);
+        return;
+    }
+
+    leftSubTree.emplace_back(root->val);
+    getLeftSubTree(root->left, leftSubTree);
+    getLeftSubTree(root->right, leftSubTree);
+}
+
+// FXN 2: Get right subtree vals as a vector - right first
+void getRightSubTree(TreeNode* root, vector<int>& rightSubTree){
+    if(root == NULL){
+        rightSubTree.emplace_back(-101);
+        return;
+    }
     
+    rightSubTree.emplace_back(root->val);
+    getRightSubTree(root->right, rightSubTree);
+    getRightSubTree(root->left, rightSubTree);
+    
+}
+
+// FXN 3: Conpare results
+bool isSymmetric(TreeNode* root) {
+    if(root == NULL) return true;
+    vector<int> leftSubTree;
+    vector<int> rightSubTree;
+
+    getLeftSubTree(root, leftSubTree);
+    getRightSubTree(root, rightSubTree);
+
+    return (leftSubTree == rightSubTree);
 }
 
 int main(){
