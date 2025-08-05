de33c54dad54fb3c93e05bee358eed2d19070db4
Joy Sarkar <joysarkar0538@gmail.com>
2025-08-05 23:24:35 +0530

    Update 904_fruitIntoBaskets.cpp

--- Leetcode/904_fruitIntoBaskets.cpp
+++ Leetcode/904_fruitIntoBaskets.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
 int totalFruit(vector<int>& fruits) {
-    
+    int n = fruits.size();
+    unordered_map<int, int> basket;
+    int maxElems = 0;
+
+    int i = 0, j = 0;
+
+    while(j<n){
+        if(basket.size() <= 2){
+            basket[fruits[j]]++;
+        }
+        while(basket.size() > 2){
+            basket[fruits[i]]--;
+            if(basket[fruits[i]] == 0) basket.erase(fruits[i]);
+            i++;
+        }
+        maxElems = max(maxElems, (j - i + 1));
+        j++;
+    }
+    return maxElems;
 }
 
 int main(){
