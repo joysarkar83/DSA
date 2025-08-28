93e51e8ee7056593cc7f8caae96485eff268eafd
Joy Sarkar <joysarkar0538@gmail.com>
2025-08-29 00:16:05 +0530

    Create 287_findTheDuplicateNumber.cpp

---
+++ Leetcode/287_findTheDuplicateNumber.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int findDuplicate(vector<int>& nums) {
+    int slow = 0, fast = 0;
+
+    while (true) {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+        if (slow == fast) {
+            break;
+        }
+    }
+
+    int slow2 = 0;
+
+    while (true) {
+        slow = nums[slow];
+        slow2 = nums[slow2];
+        if (slow == slow2) {
+            return slow;
+        }
+    }
+    return slow;
+}
+
+int main(){
+
+    return 0;
+}
